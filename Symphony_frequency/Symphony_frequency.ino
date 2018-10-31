0a1de3f15c7d72613ee59d14815ca0f970bb87b6
Sahith140699 <37544315+Sahith140699@users.noreply.github.com>
2018-10-31 22:23:05 +0530

    Add files via upload

---
+++ Symphony_frequency/Symphony_frequency.ino
@@ -0,0 +1,28 @@
+const int pin = A2;
+long getFrequency(int pin);
+
+void setup() 
+{
+  pinMode(pin,INPUT);
+  Serial.begin(115200);
+}
+
+void loop() 
+{
+  long freq = 0;
+  freq = getFrequency(pin);
+  Serial.println(freq);
+  delay(100);
+}
+
+long getFrequency(int pin)
+{
+  #define SAMPLES 4096
+  long freq = 0;
+  for(unsigned int j=0; j<SAMPLES ; j++)
+  {
+    freq += 500000/pulseIn(pin,HIGH,250000); 
+  }
+  return freq/SAMPLES;
+}
+
